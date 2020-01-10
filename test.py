621eb70c0da2771a1b5a5b282c4c56dccd47f29d
Milos Vasic <milos85vasic@gmail.com>
2020-01-10 10:30:50 +0100

    MSF-13 - Cleaning up.

--- test.py
+++ test.py
@@ -13,50 +13,78 @@ key_test_name = "name"
 key_test_type = "type"
 key_application_mail_server_factory = "Mail-Server-Factory"
 
+websetup_script = "websetup.py"
+echo_python_cmd_script = "echo_python_cmd.sh"
 configuration_file = "configuration.json"
+toolkit_directory = "Toolkit"
+toolkit_repo_raw_access = "https://raw.githubusercontent.com/milos85vasic/Apache-Factory-Toolkit/master/"
 
 def get_installation_commands(type):
     switcher = {
         key_application_mail_server_factory: 
             [
-                curl_to("https://raw.githubusercontent.com/milos85vasic/Apache-Factory-Toolkit/master/echo_python_cmd.sh", "echo_python_cmd.sh"),
-                curl_to("https://raw.githubusercontent.com/milos85vasic/Apache-Factory-Toolkit/master/websetup.py", "websetup.py"),
-                "`sh echo_python_cmd.sh` websetup.py Mail-Server-Factory"
+                curl_to(toolkit_repo_raw_access + echo_python_cmd_script, echo_python_cmd_script),
+                curl_to(toolkit_repo_raw_access + websetup_script, websetup_script),
+                "`sh " + echo_python_cmd_script + "` " + websetup_script + " " + key_application_mail_server_factory,
+            ]
+    }
+    return switcher.get(type, "echo 'Unsupported application type: " + type + "'")
+
+def get_shutdown_commands(type):
+    switcher = {
+        key_application_mail_server_factory: 
+            [
+                rm(echo_python_cmd_script)
             ]
     }
     return switcher.get(type, "echo 'Unsupported application type: " + type + "'")
 
 
-def cleanup_test():
-    #  Do cleanup the test.
-    return
+def get_cleanup_commands(type):
+    switcher = {
+        key_application_mail_server_factory: 
+            [
+                rm(toolkit_directory),
+                rm(websetup_script),
+                rm(echo_python_cmd_script)
+            ]
+    }
+    return switcher.get(type, "echo 'Unsupported application type: " + type + "'")
+
+
+def append_command(steps, ssh_access, command):
+    to_execute = ssh(
+            ssh_access[key_user], 
+            "'" + command + "'",
+            port=ssh_access[key_port], 
+            host=ssh_access[key_host]
+    )
+    steps.append(to_execute)
+    print("Will execute: ", to_execute)
 
 
 def run_test():
-    cleanup_test()
-
     if not path.exists(configuration_file):
         sys.exit("Configuration file is not available.")
         return
 
     data = json.load(open(configuration_file))
+    ssh_access = data[key_ssh]
 
+    steps = []
     for test in data[key_tests]:
-        steps = []
+        print("Executing test:", test[key_test_name])
+        for command in get_cleanup_commands(test[key_test_type]):
+            append_command(steps, ssh_access, command)
 
         for command in get_installation_commands(test[key_test_type]):
-            to_execute = ssh(
-                    data[key_ssh][key_user], 
-                    "'" + command + "'",
-                    port=data[key_ssh][key_port], 
-                    host=data[key_ssh][key_host]
-            )
-            steps.append(to_execute)
-            print("Will execute: ", to_execute)
+            append_command(steps, ssh_access, command)
 
-        print("Executing:", test[key_test_name])
+        for command in get_shutdown_commands(test[key_test_type]):
+            append_command(steps, ssh_access, command)
+        
         run(steps)
-        print("Executed:", test[key_test_name])
+        print("Test executed:", test[key_test_name])
 
 
 if __name__ == '__main__':
